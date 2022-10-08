190092f336a4716143278a09b8d0a87bd3f1a113
Sejo Basic <sejoob@gmail.com>
2022-10-07 23:34:51 -0700

    Header section styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,26 @@ import '../styling/Header.css'
 function Header() {
   return (
     <div className='header'>
-      <header className="header-container">
-        <h1>SEJO BASIC<br /> CREATIVE DEVELOPER &<br />&nbsp;DESIGNER</h1>
-      </header>
+      <div className='header-container'>
+        <h1>SEJO BASIC</h1>
+        <p>
+          HI MY NAME IS SEJO, I AM A SOFTWARE ENGINEER AND UI/GRAPHIC DESIGNER
+          BASED IN ARIZONA. I SPECIALIZE IN FRONT-END DEVELOPMENT, CREATIVE
+          DESIGN AND USER EXPERIENCE. MY PASSION FOR CREATIVITY MOTIVATES ME TO
+          CONTINUOUSLY EXPAND MY SKILL SET THROUGH NEW TECHNOLOGIES, INSPIRE
+          OTHERS, AND WORK COLLECTIVELY TO BRING NEW IDEAS TO LIFE.
+        </p>
+      </div>
+      <div className="divider"></div>
+      <div className='title-container'>
+        <span>Creative Developer</span>
+        <span>+</span>
+        <span>Designer</span>
+        <span>+</span>
+        <span>Musician</span>
+        <span>+</span>
+        <span>Artist</span>
+      </div>
     </div>
   )
 }
