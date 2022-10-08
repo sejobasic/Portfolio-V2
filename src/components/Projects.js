c55525644e005373628f5cf7991a55c594018ac6
Sejo Basic <sejoob@gmail.com>
2022-10-07 18:29:07 -0700

    Projects section boiler complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// Project images
+import filmList from '../assets/film.png'
+import layr from '../assets/layr.png'
+import simple from '../assets/simple.png'
+import trails from '../assets/trails.png'
+
 function Projects() {
   return (
     <>
@@ -10,12 +16,108 @@ function Projects() {
         </div>
       </div>
       <div className='divider'></div>
-      
-      <section className='projects-section'>
-        <div className='projects-container'></div>
+
+      <section className='project-section'>
+        <div className='project-container'>
+          <div>
+            <nav className='menu'>
+              <div className='menu-item'>
+                <a
+                  href='https://filmlist-project.netlify.app/'
+                  className='menu-item-link'
+                >
+                  / FILM LIST
+                </a>
+                <img
+                  src={filmList}
+                  alt='film list project'
+                  className='menu-item-img'
+                />
+                <div className='marquee'>
+                  <div className='marquee-inner'>
+                    <span>FILM LIST</span>
+                    <span>FILM LIST</span>
+                    <span>FILM LIST</span>
+                    <span>FILM LIST</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className='divider'></div>
+
+              <div className='menu-item'>
+                <a
+                  href='https://filmlist-project.netlify.app/'
+                  className='menu-item-link'
+                >
+                  / LAYR
+                </a>
+                <img src={layr} alt='layr project' className='menu-item-img' />
+                <div className='marquee'>
+                  <div className='marquee-inner'>
+                    <span>LAYR</span>
+                    <span>LAYR</span>
+                    <span>LAYR</span>
+                    <span>LAYR</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className='divider'></div>
+
+              <div className='menu-item'>
+                <a
+                  href='https://filmlist-project.netlify.app/'
+                  className='menu-item-link'
+                >
+                  / SIMPLE FRIDGE
+                </a>
+                <img
+                  src={simple}
+                  alt='simple fridge project'
+                  className='menu-item-img'
+                />
+                <div className='marquee'>
+                  <div className='marquee-inner'>
+                    <span>SIMPLE FRIDGE</span>
+                    <span>SIMPLE FRIDGE</span>
+                    <span>SIMPLE FRIDGE</span>
+                    <span>SIMPLE FRIDGE</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className='divider'></div>
+
+              <div className='menu-item'>
+                <a
+                  href='https://filmlist-project.netlify.app/'
+                  className='menu-item-link'
+                >
+                  / TRAILS
+                </a>
+                <img
+                  src={trails}
+                  alt='trails project'
+                  className='menu-item-img'
+                />
+                <div className='marquee'>
+                  <div className='marquee-inner'>
+                    <span>TRAILS</span>
+                    <span>TRAILS</span>
+                    <span>TRAILS</span>
+                    <span>TRAILS</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className='divider'></div>
+            </nav>
+          </div>
+        </div>
       </section>
 
-      <div className="whitespace"></div>
+      <div className='whitespace'></div>
     </>
   )
 }
