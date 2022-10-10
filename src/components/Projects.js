bf33c1751cfb6c990b23e8357a7f7ba3f82747cc
Sejo Basic <sejoob@gmail.com>
2022-10-10 15:36:29 -0700

    Code cleanup --projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import projectData from '../utils/projectData'
 import '../styling/Projects.css'
 
 // Project images
@@ -22,6 +23,42 @@ function Projects() {
     },
   }
 
+  const renderProjectData = projectData.map((project) => {
+    return (
+      <>
+        <motion.div
+          className='menu-item'
+          initial='hidden'
+          animate='visible'
+          variants={projectVariant}
+        >
+          <a
+            href={project.link}
+            target='_blank'
+            rel='noreferrer'
+            className='menu-item-link'
+          >
+            {project.title}
+          </a>
+          <img
+            src={project.image}
+            alt={project.title + 'project image'}
+            className='menu-item-img'
+          />
+          <div className='marquee'>
+            <div className='marquee-inner'>
+              <span>{project.title}</span>
+              <span>{project.title}</span>
+              <span>{project.title}</span>
+              <span>{project.title}</span>
+            </div>
+          </div>
+        </motion.div>
+        <Divider />
+      </>
+    )
+  })
+
   return (
     <>
       <Section
@@ -34,128 +71,7 @@ function Projects() {
       <section className='project-section'>
         <div className='project-container'>
           <div>
-            <nav className='menu'>
-              <motion.div
-                className='menu-item'
-                initial='hidden'
-                animate='visible'
-                variants={projectVariant}
-              >
-                <a
-                  href='https://filmlist-project.netlify.app/'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='menu-item-link'
-                >
-                  ✣ FILM LIST
-                </a>
-                <img
-                  src={filmList}
-                  alt='film list project'
-                  className='menu-item-img'
-                />
-                <div className='marquee'>
-                  <div className='marquee-inner'>
-                    <span>FILM LIST</span>
-                    <span>FILM LIST</span>
-                    <span>FILM LIST</span>
-                    <span>FILM LIST</span>
-                  </div>
-                </div>
-              </motion.div>
-
-              <Divider />
-
-              <motion.div
-                className='menu-item'
-                initial='hidden'
-                animate='visible'
-                variants={projectVariant}
-              >
-                <a
-                  href='http://www.layrsequencer.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='menu-item-link'
-                >
-                  ✣ LAYR SEQUENCER
-                </a>
-                <img src={layr} alt='layr project' className='menu-item-img' />
-                <div className='marquee'>
-                  <div className='marquee-inner'>
-                    <span>LAYR SEQUENCER</span>
-                    <span>LAYR SEQUENCER</span>
-                    <span>LAYR SEQUENCER</span>
-                    <span>LAYR SEQUENCER</span>
-                  </div>
-                </div>
-              </motion.div>
-
-              <Divider />
-
-              <motion.div
-                className='menu-item'
-                initial='hidden'
-                animate='visible'
-                variants={projectVariant}
-              >
-                <a
-                  href='https://github.com/weiwei-out/MagicFridge'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='menu-item-link'
-                >
-                  ✣ SIMPLE FRIDGE
-                </a>
-                <img
-                  src={simple}
-                  alt='simple fridge project'
-                  className='menu-item-img'
-                />
-                <div className='marquee'>
-                  <div className='marquee-inner'>
-                    <span>SIMPLE FRIDGE</span>
-                    <span>SIMPLE FRIDGE</span>
-                    <span>SIMPLE FRIDGE</span>
-                    <span>SIMPLE FRIDGE</span>
-                  </div>
-                </div>
-              </motion.div>
-
-              <Divider />
-
-              <motion.div
-                className='menu-item'
-                initial='hidden'
-                animate='visible'
-                variants={projectVariant}
-              >
-                <a
-                  href='https://github.com/sejobasic/Trails-app'
-                  target='_blank'
-                  rel='noreferrer'
-                  className='menu-item-link'
-                >
-                  ✣ TRAILS
-                </a>
-                <img
-                  src={trails}
-                  alt='trails project'
-                  className='menu-item-img'
-                />
-                <div className='marquee'>
-                  <div className='marquee-inner'>
-                    <span>TRAILS</span>
-                    <span>TRAILS</span>
-                    <span>TRAILS</span>
-                    <span>TRAILS</span>
-                    <span>TRAILS</span>
-                  </div>
-                </div>
-              </motion.div>
-
-              <Divider />
-            </nav>
+            <nav className='menu'>{renderProjectData}</nav>
           </div>
         </div>
       </section>
---
+++ src/utils/projectData.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+
+  {
+    id: 1,
+    title: '✣ FILM LIST',
+    image: require('../assets/film.png'),
+    link: 'https://filmlist-project.netlify.app/'
+  },
+  {
+    id: 2,
+    title: '✣ LAYR SEQUENCER',
+    image: require('../assets/layr.png'),
+    link: 'http://www.layrsequencer.com/'
+  },
+  {
+    id: 3,
+    title: '✣ SIMPLE FRIDGE',
+    image: require('../assets/simple.png'),
+    link: 'https://github.com/weiwei-out/MagicFridge'
+  },
+  {
+    id: 4,
+    title: '✣ TRAILS',
+    image: require('../assets/trails.png'),
+    link: 'https://github.com/sejobasic/Trails-app'
+  },
+]
