25ec4706d0f609321141483362de04b48906101b
Sejo Basic <sejoob@gmail.com>
2023-01-18 08:56:25 -0700

    Fixed footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/Footer.css'
 function Footer() {
   return (
     <div className='footer-container'>
-      <span>created by sejo basic</span>
+      <span>developed by sejo basic</span>
       <span>2023</span>
     </div>
   )
